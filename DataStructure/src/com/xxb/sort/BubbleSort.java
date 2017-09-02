010d3dc5a8042b9a17751367465a785762dafa44
reunion07 <reunion7@163.com>
2017-09-02 16:17:50 +0800

    test
    Signed-off-by: reunion07 <reunion7@163.com>

--- DataStructure/src/com/xxb/sort/BubbleSort.java
+++ DataStructure/src/com/xxb/sort/BubbleSort.java
@@ -2,15 +2,9 @@ package com.xxb.sort;
 
 import org.junit.Test;
 
-/**
- * 冒泡排序
- * 时间复杂度 O(n2)
- * @author 谢小波
- *
- */
+
 public class BubbleSort {
 
-	//交换
 	public void swap(int[] list, int i, int j){
 		int temp = list[i];
 		list[i] = list[j];
@@ -34,10 +28,7 @@ public class BubbleSort {
 //			System.out.print(list[i]+" ");
 //		}
 	}
-	/*
-	 * 第二种相对第一种，做了一些优化。
-	 * 体现在：第一种可能随着每一个数的冒泡它会把一些小的数 移动到最后，效率非常低
-	 */
+
 	public void bubbleSort01(int[] list){
 		
 		for(int i=0; i<list.length; i++){
@@ -59,6 +50,6 @@ public class BubbleSort {
 		System.out.println(list.length);
 		//bubbleSort(list);
 		bubbleSort01(list);
-		
+		//澶у偦閫糳ashabi
 	}
 }
